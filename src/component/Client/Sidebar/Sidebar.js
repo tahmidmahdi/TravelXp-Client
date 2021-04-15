53825b01bb718b35cc41783d628dc199f1e154c9
tahmidmahdi <tahmid5550@gmail.com>
2021-04-15 23:56:12 +0600

    booking is making

--- src/component/Client/Sidebar/Sidebar.js
+++ src/component/Client/Sidebar/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css'
+import cart from '../../../images/shopping-cart.svg'
+import list from '../../../images/price-list.svg'
+import rating from '../../../images/rating.svg'
 
 const Sidebar = () => {
     return (
@@ -8,11 +11,11 @@ const Sidebar = () => {
 
             <Link className='link' to="/home"><h3>Travel XP</h3></Link>
             <br/><br/>
-            <h6>Book</h6>
+            <Link className='link' to="/booking"><h6><img style={{height:'35px'}}  src={cart} alt=""/> Book</h6></Link>
             <br/>
-            <h6>Booking List</h6>
+           <Link className='link' to='/bookinglist'> <h6><img style={{height:'35px'}} src={list} alt=""/> Booking List</h6></Link>
             <br/>
-            <h6>Review</h6>
+            <Link className='link' to='/review'><h6><img style={{height:'35px'}} src={rating} alt=""/> Review</h6></Link>
         </div>
     );
 };
